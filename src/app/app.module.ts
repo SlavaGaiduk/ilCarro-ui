82322e0158106e61ade036f3ef023b6ad9873c75
shtruk <shtruk@yandex-team.ru>
2019-12-04 12:11:06 +0300

    app-bootstrap change import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { IcUiModule } from '../../projects/ic-ui/src/lib/ic-ui.module';
+import { IcUiModule } from '../../projects/ic-ui/src/public-api';
 
 @NgModule({
   declarations: [
