f9aef2579b49d1d7ce70bb82732f920841b8df01
KT <kaytvy@gmail.com>
2019-12-09 21:20:02 +0200

    Added css of buttons, slider and checkbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Options } from 'ng5-slider';
+import {faSearch, faSlidersH, faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss', '../styles/il-slider.scss']
 })
 export class AppComponent {
-  title = 'ilCargo';
+  title = 'ilCarro';
+  faSearch = faSearch;
+  faSlidersH =faSlidersH;
+  faMapMarkerAlt = faMapMarkerAlt;
+
+// @Component({
+//   selector: 'app-styled-slider',
+//   templateUrl: './styled-slider.component.html',
+//   styleUrls: ['./styled-slider.component.scss']
+// })
+// export class StyledSliderComponent {
+  minValue: number = 10;
+  maxValue: number = 90;
+  options: Options = {
+    floor: 0,
+    ceil: 100,
+    step: 10,
+    showTicks: true
+  };
 }
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IcUiModule } from '../../projects/ic-ui/src/public-api';
+import {Ng5SliderModule} from "ng5-slider";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    IcUiModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-  ],
+    imports: [
+        BrowserModule,
+        IcUiModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        Ng5SliderModule,
+        MatButtonToggleModule,
+      FontAwesomeModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
