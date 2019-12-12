93a7415a552b1f5f2b3400fbf245bfb6cb9e2508
NikolayMalikhin <nikolay.malykhin@septagon.ru>
2019-12-12 18:00:17 +0200

    tabs change order

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Options } from 'ng5-slider';
+import { FormControl } from '@angular/forms';
 
 @Component({
 	selector: 'app-root',
@@ -9,10 +10,33 @@ import { Options } from 'ng5-slider';
 export class AppComponent {
 	title = 'ilCargo';
 	// slider
-	sliderMinValue: number = 100;
-	sliderMaxValue: number = 300;
+	sliderMinValue: number;
+	sliderMaxValue: number;
 	options: Options = {
 		floor: 0,
 		ceil: 600
 	};
+
+	constructor() {
+		this.sliderMinValue = 100;
+		this.sliderMaxValue = 300;
+	}
+
+	// tabs
+	tabs = ['Search', 'Filters', 'Map'];
+	selected = new FormControl(0);
+
+	changeTabOrder(tabNumber: number) {
+		switch (this.tabs[tabNumber]) {
+			case 'Search':
+				this.tabs = ['Search', 'Filters', 'Map'];
+				break;
+			case 'Filters':
+				this.tabs = ['Filters', 'Map', 'Search'];
+				break;
+			case 'Map':
+				this.tabs = ['Map', 'Search', 'Filters'];
+				break;
+		}
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IcUiModule } from '../../projects/ic-ui/src/public-api';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, IcUiModule, AppRoutingModule, BrowserAnimationsModule],
+	imports: [BrowserModule, IcUiModule, AppRoutingModule, BrowserAnimationsModule, ReactiveFormsModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
