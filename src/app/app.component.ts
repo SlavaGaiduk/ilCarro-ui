b1c19073374f90312b9345720990af0360996ec8
NikolayMalikhin <nikolay.malykhin@septagon.ru>
2019-12-18 17:34:06 +0200

    tabs css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,9 +21,9 @@ export class AppComponent {
 	// tabs
 	selected = new FormControl(0);
 	tabs = [
-		{ label: 'Search', icon: faSearch },
-		{ label: 'Filters', icon: faSlidersH },
-		{ label: 'Map', icon: faMapMarkedAlt }
+		{ label: 'Search', icon: faSearch, color: 'cl1', transform: 'trans-active', active: 'active1' },
+		{ label: 'Filters', icon: faSlidersH, color: 'cl2', transform: 'trans', active: 'active2' },
+		{ label: 'Map', icon: faMapMarkedAlt, color: 'cl3', transform: 'trans', active: 'active3' }
 	];
 
 	constructor() {
@@ -33,7 +33,14 @@ export class AppComponent {
 
 	changeTabOrder($event: number) {
 		const tab = this.tabs[$event];
+		tab.transform = 'trans-active';
+		this.tabs[0].transform = 'trans';
 		this.tabs.splice($event, 1);
 		this.tabs.splice(0, 0, tab);
+
+		document.getElementById('mat-tab-label-0-0').classList.remove('active1');
+		document.getElementById('mat-tab-label-0-0').classList.remove('active2');
+		document.getElementById('mat-tab-label-0-0').classList.remove('active3');
+		document.getElementById('mat-tab-label-0-' + $event).classList.add(this.tabs[0].active);
 	}
 }
