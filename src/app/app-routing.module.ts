11426148d89dd5cd7e43f9f880a5dbca7cd88b99
NikolayMalikhin <nikolay.malykhin@septagon.ru>
2019-12-23 15:31:14 +0200

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [{ path: 'dev-ui', loadChildren: './pages/dev-ui/dev-ui.module#DevUiModule' }];
+const routes: Routes = [
+	{ path: '', pathMatch: 'full', redirectTo: 'login' },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'dev-ui', loadChildren: './pages/dev-ui/dev-ui.module#DevUiModule' }
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IcUiModule } from '../../projects/ic-ui/src/public-api';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, LoginComponent],
 	imports: [BrowserModule, IcUiModule, AppRoutingModule, BrowserAnimationsModule],
 	providers: [],
 	bootstrap: [AppComponent]
