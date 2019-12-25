515aa8cd000eaa6769b4b9950fc76f7d87f533f9
NikolayMalikhin <nikolay.malykhin@septagon.ru>
2019-12-25 13:22:18 +0200

    most popular cars

--- src/app/pages/login/most-popular-choices/most-popular-choices.component.ts
+++ src/app/pages/login/most-popular-choices/most-popular-choices.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-most-popular-choices',
 	templateUrl: './most-popular-choices.component.html',
 	styleUrls: ['./most-popular-choices.component.scss']
 })
-export class MostPopularChoicesComponent implements OnInit {
-	constructor() {}
+export class MostPopularChoicesComponent {
+	private cars: any[];
 
-	ngOnInit() {}
+	constructor() {
+		this.cars = [
+			{ name: 'reddevil', symbol: '$', sum: '230', period: 'perday', imgUrl: '/assets/images/zaz.png' },
+			{ name: 'blackstar', symbol: '$', sum: '300', period: 'perday', imgUrl: '/assets/images/gaz.png' },
+			{ name: 'pneumodaddy', symbol: '$', sum: '180', period: 'perday', imgUrl: '/assets/images/azlk.png' }
+		];
+	}
+
+	getParallaxImage(imgUrl: string) {
+		return 'url(' + imgUrl + ')';
+	}
 }
