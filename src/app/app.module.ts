331f33b791b78c913a8ba8ec3ae6380e8a94bf09
VictoriaVend <42913694+VictoriaVend@users.noreply.github.com>
2019-12-25 14:10:59 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IcUiModule } from '../../projects/ic-ui/src/public-api';
+import { ToolsComponent } from './components/tools/tools.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { MatCheckboxModule } from '@angular/material';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, IcUiModule, AppRoutingModule, BrowserAnimationsModule],
+	declarations: [AppComponent, ToolsComponent, SignUpComponent],
+	imports: [BrowserModule, IcUiModule, AppRoutingModule, BrowserAnimationsModule, MatSliderModule, MatCheckboxModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
